fc0bf92e284956645c71e03c44bf407b6776f9d8
t_hrithik <hrithik.ranjan@lavainternational.in>
2025-03-20 18:16:14 +0530

    Retrofit instance created and apiservice setup done, Model created

---
+++ app/src/main/java/com/rkdigital/filmfolio/serviceapi/MovieApiService.java
@@ -0,0 +1,13 @@
+package com.rkdigital.filmfolio.serviceapi;
+
+import com.rkdigital.filmfolio.model.Result;
+
+import retrofit2.Call;
+import retrofit2.http.GET;
+import retrofit2.http.Query;
+
+public interface MovieApiService {
+
+    @GET("movie/popular")
+    Call<Result>getPopularMovies(@Query("api_key") String apiKey);
+}
