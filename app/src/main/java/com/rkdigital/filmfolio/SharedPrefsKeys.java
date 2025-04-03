7807d5f53a1074d13adf72ca891c27efa7a81fb0
t_hrithik <hrithik.ranjan@lavainternational.in>
2025-04-03 18:20:10 +0530

    Functionality of bottom sheet dialog added. Chip showing odd behaviour.

--- app/src/main/java/com/rkdigital/filmfolio/SharedPrefsKeys.java
+++ app/src/main/java/com/rkdigital/filmfolio/SharedPrefsKeys.java
@@ -5,6 +5,9 @@ public interface SharedPrefsKeys {
     String IS_LOGGED_IN         = "isLoggedIn";
     String USER_NAME            = "userName";
     String USER_ID              = "userId";
-    String FILTER               =  "filter";
-    String FILTER_GENRES = "filter_genres";
+    String KEY_GENRES = "filter_genres";
+    String KEY_SORT = "filter_sort";
+    String KEY_MIN_YEAR = "filter_min_year";
+    String KEY_MAX_YEAR = "filter_max_year";
+    String KEY_MIN_RATING = "filter_min_rating";
 }
